17d6cbac5508587d8431a6c5f573925cc8a02e57
ensalada.de.pollo <ensalada.de.pollo@90.local>
2025-05-15 19:02:57 +0900

    key.json 수정

--- src/main/java/org/example/safetyconnection/config/FirebaseConfig.java
+++ src/main/java/org/example/safetyconnection/config/FirebaseConfig.java
@@ -3,23 +3,37 @@ package org.example.safetyconnection.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
-    @Bean
-    public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("/SafetyConnection-Server-Sub/serviceAccountKey.json");
+    @Value("${firebase.service-account}")
+    private String serviceAccountKey;
 
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
+    @PostConstruct
+    public void init() {
+        try {
+            InputStream serviceAccount = new ByteArrayInputStream(getBase64DecodeBytes(serviceAccountKey));
+            FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
 
-        return FirebaseApp.initializeApp(options);
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static byte[] getBase64DecodeBytes(String input) {
+        return Base64.decodeBase64(input);
     }
 }
